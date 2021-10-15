9e8c94f48b3a5165367d229df314fc558cec98c6
metanivek <metanivek@gmail.com>
2021-10-15 15:10:47 -0400

    fix pdf alignment

--- components/Pdf/index.js
+++ components/Pdf/index.js
@@ -1,22 +1,21 @@
 import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 export default function Pdf({ item }) {
   // TODO centralize gateway stuff
   const gateway = "https://metanivek.mypinata.cloud/ipfs/";
   const uri = item.artifact_uri.replace("ipfs://", gateway);
 
-  console.log("pdf uri: ", uri);
   const [numPages, setNumPages] = useState(null);
 
   function onDocumentLoadSuccess({ numPages: nextNumPages }) {
     setNumPages(nextNumPages);
   }
+  // TODO: handle pagination
   return (
-    // TODO: handle pagination
-    <Box align="center">
+    <Flex direction="column" align="center" justify="center" grow={1}>
       <Document
         options={{
           cMapUrl: "cmaps/",
@@ -29,6 +28,6 @@ export default function Pdf({ item }) {
           <Page key={`page_${index + 1}`} pageNumber={index + 1} />
         ))}
       </Document>
-    </Box>
+    </Flex>
   );
 }
