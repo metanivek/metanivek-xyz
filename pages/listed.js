aa278b37285ffe27b20a14a66f0041bb86215ce8
metanivek <metanivek@gmail.com>
2021-11-12 14:48:34 -0500

    primary and secondary counts

--- pages/listed.js
+++ pages/listed.js
@@ -2,17 +2,23 @@ import { Text, VStack } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Gallery from "../components/Gallery";
 import { fetchAllItemsHoc } from "../lib/pages";
-import { filterListedObjkts } from "../lib/objkt";
+import { filterListedObjkts, primaryCount, secondaryCount } from "../lib/objkt";
+
+const pluralize = function (number, singular, plural) {
+  return `${number} ${number > 1 ? singular : plural}`;
+};
 
 const Listed = ({ items }) => {
   items = filterListedObjkts(items);
+  const primary = primaryCount(items);
+  const secondary = secondaryCount(items);
   return (
     <VStack>
       <Header />
       {items.length > 0 && (
         <Text padding={[4, 4, 4, 8]} textAlign="center">
-          There are currently {items.length} metanivek{" "}
-          {items.length > 1 ? "pieces" : "piece"} available to collect.
+          There are currently {pluralize(primary, "piece", "pieces")} on primary
+          and {pluralize(secondary, "piece", "pieces")} on secondary markets.
         </Text>
       )}
       <Gallery
